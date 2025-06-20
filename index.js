839e88b19cb01f6cc7d1199d8f9be3ee8dc2b9ad
Lucas de Moura dos Santos <lucasmr910@gmail.com>
2025-06-20 17:15:39 -0300

    Fix index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const player1 = {
+    NOME: "Mário",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 2,
+    PONTOS: 0,
+};
+
+const player3 = {
+    NOME: "Peach",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 2,
+    PONTOS: 0,
+};
+
+const player4 = {
+    NOME: "Bowser",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 3,
+    PODER: 5,
+    PONTOS: 0,
+};
+
+const player5 = {
+    NOME: "Yoshi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 5,
+    PODER: 2,
+    PONTOS: 0,
+};
+
+const player6 = {
+    NOME: "Toad",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 2,
+    PODER: 4,
+    PONTOS: 0,
+};
+
+const player7 = {
+    NOME: "Donk Kong",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+};
